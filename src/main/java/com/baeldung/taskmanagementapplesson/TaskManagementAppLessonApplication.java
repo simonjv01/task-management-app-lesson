e29eef58ab9d85086917822b32f1b9633c06725b
Simon Vargas <sjvargas01@gmail.com>
2021-09-04 19:15:47 -0400

    first commit

---
+++ src/main/java/com/baeldung/taskmanagementapplesson/TaskManagementAppLessonApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.taskmanagementapplesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementAppLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeldung/taskmanagementapplesson/TaskManagementAppLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.taskmanagementapplesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
